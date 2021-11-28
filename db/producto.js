7a34424e642f2928795395c2350ed9802c83e316
gungla <gglahn@gmail.com>
2021-11-28 12:35:57 -0300

    DAO DTO

---
+++ db/producto.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const productosCollection = 'productos';
+
+const ProductosSchema = new mongoose.Schema({
+    id: {type: Number, require: true},
+    nombre: {type: String, require: true, max: 100},
+    codigo: {type: Number, require: true, max: 20},
+    precio: {type: Number, require: true},
+    stock: {type: Number, require: true}
+})
+
+module.exports = mongoose.model(productosCollection, ProductosSchema);
